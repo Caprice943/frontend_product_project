ac10c9e44c06fb35f08ff167401324c6a059cfc4
Caprice943 <heredyamouiri@gmail.com>
2023-09-17 22:34:46 +0200

    frontend without api

---
+++ src/app/add-produit/add-produit.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+import { Categorie } from '../model/categorie.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-produit',
+  templateUrl: './add-produit.component.html',
+  styleUrls: ['./add-produit.component.css'],
+})
+export class AddProduitComponent implements OnInit {
+  newProduit = new Produit();
+  message: string | undefined;
+
+  categories! : Categorie[];
+  newIdCat! : number;
+  newCategorie! : Categorie;
+
+constructor(private produitService: ProduitService,
+private router :Router) { }
+ 
+
+ngOnInit() {
+  this.categories = this.produitService.listeCategories();
+  }
+
+  addProduit() {
+    // console.log(this.newProduit);
+    this.produitService.ajouterProduit(this.newProduit);
+    this.message = 'Le produit ' + this.newProduit.nomProduit + ' a été ajouté avec succès !';
+    
+    this.newCategorie = this.produitService.consulterCategorie(this.newIdCat);
+    this.newProduit.categorie = this.newCategorie;
+    this.produitService.ajouterProduit(this.newProduit);
+    this.router.navigate(['produits']);
+  }
+}
