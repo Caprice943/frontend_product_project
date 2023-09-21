f1b98cb2d75baf81d9559a55b3920c23da714f49
Caprice943 <heredyamouiri@gmail.com>
2023-09-22 00:41:22 +0200

    add search component and update navigation

---
+++ src/app/search-by-produit/search-by-produit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-by-produit',
+  templateUrl: './search-by-produit.component.html',
+  styleUrls: ['./search-by-produit.component.css']
+})
+export class SearchByProduitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
